877f35771dddb1ea14b21f29633a6e13367a2710
adenhilde <adenh04@icloud.com>
2025-10-19 23:02:29 -0700

    barebones

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define NUMARGS 3
+
+char usage[] = "\
+usage: zipper [ -z / -u ] [input] [output] \n \
+file compression algorithm \n \
+written in C \n \
+Aden Hilderbrand\n";
+
+int main(int argc, char *argv[]){
+        if (argc < NUMARGS) {
+                printf("%s", usage);
+                return 0;
+        }
+
+        return 0;
+}
